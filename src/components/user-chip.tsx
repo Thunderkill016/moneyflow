e10167042615350f88f567100f1c866a62ed1473
Thunderkill <66909862+Thunderkill016@users.noreply.github.com>
2026-07-25 19:15:23 +0700

    perf: route product styles through the app entry

--- src/components/user-chip.tsx
+++ src/components/user-chip.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import "@/app/ui-refresh.css";
+import "@/app/product-styles";
 import { signOut } from "@/app/(auth)/actions";
 import { Icon } from "@/components/icons";
 import {
